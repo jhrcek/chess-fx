7e2eafe6c64dce4f6af20c4eaff4bc7c4492c4b5
Jan Hrcek <jhrcek@redhat.com>
2014-06-09 08:09:49 +0200

    Add isWhite() to Piece

--- src/main/java/cz/janhrcek/chess/model/Piece.java
+++ src/main/java/cz/janhrcek/chess/model/Piece.java
@@ -9,18 +9,18 @@ import java.util.Set;
  */
 public enum Piece {
 
-    WHITE_PAWN("P", ""),
-    WHITE_KNIGHT("N", "N"),
-    WHITE_BISHOP("B", "B"),
-    WHITE_ROOK("R", "R"),
-    WHITE_QUEEN("Q", "Q"),
-    WHITE_KING("K", "K"),
-    BLACK_PAWN("p", ""),
-    BLACK_KNIGHT("n", "N"),
-    BLACK_BISHOP("b", "B"),
-    BLACK_ROOK("r", "R"),
-    BLACK_QUEEN("q", "Q"),
-    BLACK_KING("k", "K");
+    WHITE_PAWN(true, "P", ""),
+    WHITE_KNIGHT(true, "N", "N"),
+    WHITE_BISHOP(true, "B", "B"),
+    WHITE_ROOK(true, "R", "R"),
+    WHITE_QUEEN(true, "Q", "Q"),
+    WHITE_KING(true, "K", "K"),
+    BLACK_PAWN(false, "p", ""),
+    BLACK_KNIGHT(false, "n", "N"),
+    BLACK_BISHOP(false, "b", "B"),
+    BLACK_ROOK(false, "r", "R"),
+    BLACK_QUEEN(false, "q", "Q"),
+    BLACK_KING(false, "k", "K");
 
     private static final Random RANDOM = new Random();
     private static final Piece[] PIECES = {
@@ -29,11 +29,13 @@ public enum Piece {
     };
     private static final String FEN_LETTERS = "PNBRQKpnbrqk";
 
+    private final boolean isWhite;
     private final String fen; // Shortcut for piece name used in Forsythe-Edwards notation
     private final String san; // Shortcut for piece name used in Short Algebraic notation
     private long[] canGoBitBoards;
 
-    private Piece(String fenLetter, String sanLetter) {
+    private Piece(boolean isWhite, String fenLetter, String sanLetter) {
+        this.isWhite = isWhite;
         this.fen = fenLetter;
         this.san = sanLetter;
     }
@@ -52,6 +54,10 @@ public enum Piece {
             throw new IllegalArgumentException("Invalid FEN letter: '" + fenLetter + "'");
         }
         return PIECES[pieceIndex];
+    }
+
+    public boolean isWhite() {
+        return isWhite;
     }
 
     public static Piece getRandomPiece() {
