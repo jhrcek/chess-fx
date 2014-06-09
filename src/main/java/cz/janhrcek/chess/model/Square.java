ddca62605bf62ffc977a223264ddb40aedc1f875
Jan Hrcek <jhrcek@redhat.com>
2014-06-09 18:23:56 +0200

    Implement efficient means of square iteration

--- src/main/java/cz/janhrcek/chess/model/Square.java
+++ src/main/java/cz/janhrcek/chess/model/Square.java
@@ -1,6 +1,8 @@
 package cz.janhrcek.chess.model;
 
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -34,6 +36,21 @@ public enum Square {
         A1, B1, C1, D1, E1, F1, G1, H1
     };
 
+    /* Maps from square index and direction index to the array of squares going out of that square in that direction. */
+    private static final Square[][][] ITERATION_INDEX = new Square[Square.values().length][8][];
+
+    static {
+        RayGenerator[] generators = {
+            new NorthRayGenerator(), new NorthEastRayGenerator(), new EastRayGenerator(), new SouthEastRayGenerator(),
+            new SouthRayGenerator(), new SouthWestRayGenerator(), new WestRayGenerator(), new NorthWestRayGenerator()
+        };
+        for (Square from : Square.values()) {
+            for (int i = 0; i < generators.length; i++) {
+                ITERATION_INDEX[from.getIndex()][i] = generators[i].generateRayFrom(from);
+            }
+        }
+    }
+
     private Square() {
         bitboard = 1L << (63 - ordinal());
         fileIndex = ordinal() % 8;
@@ -57,6 +74,19 @@ public enum Square {
         return ordinal();
     }
 
+    public Square[] squaresTo(Direction direction) {
+        //TODO - return just a copy of this, so that clients can't modify it!
+        return ITERATION_INDEX[ordinal()][direction.ordinal()];
+    }
+
+    /**
+     * Signifies direction for square iteration starting from given square. NORTH means up the board (towards 8th rank).
+     */
+    public enum Direction {
+
+        NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST;
+    }
+
     /* Package private implementation */
     static Square valueOf(int index) {
         return SQUARES[index];
@@ -74,5 +104,121 @@ public enum Square {
             }
         }
         return result;
+    }
+
+    /* Generation of ITERATION INDEX. Based on the idea, that each ray in given direciton (N, NE, E, SE S, SW, W NW)
+     from given square by adding fixed offset to the previous square's index until we hit the edge of the board.*/
+    private static abstract class RayGenerator {
+
+        int offset;
+        int current;
+
+        Square[] generateRayFrom(Square s) {
+            List<Square> ray = new ArrayList<>();
+            current = s.getIndex();
+            while (hasNext()) {
+                current = current + offset;
+                ray.add(SQUARES[current]);
+            }
+            return ray.toArray(new Square[0]);
+        }
+
+        abstract boolean hasNext();
+    }
+
+    private static class NorthRayGenerator extends RayGenerator {
+
+        {
+            offset = -8;
+        }
+
+        @Override
+        boolean hasNext() {
+            return current + offset >= 0;
+        }
+    }
+
+    private static class NorthEastRayGenerator extends RayGenerator {
+
+        {
+            offset = -7;
+        }
+
+        @Override
+        boolean hasNext() {
+            return current + offset >= 0 && SQUARES[current + offset].getFileIndex() != 0;
+        }
+    }
+
+    private static class EastRayGenerator extends RayGenerator {
+
+        {
+            offset = 1;
+        }
+
+        @Override
+        boolean hasNext() {
+            return current + offset < 64 && SQUARES[current + offset].getFileIndex() != 0;
+        }
+    }
+
+    private static class SouthEastRayGenerator extends RayGenerator {
+
+        {
+            offset = 9;
+        }
+
+        @Override
+        boolean hasNext() {
+            return current + offset < 64 && SQUARES[current + offset].getFileIndex() != 0;
+        }
+    }
+
+    private static class SouthRayGenerator extends RayGenerator {
+
+        {
+            offset = 8;
+        }
+
+        @Override
+        boolean hasNext() {
+            return current + offset < 64;
+        }
+    }
+
+    private static class SouthWestRayGenerator extends RayGenerator {
+
+        {
+            offset = 7;
+        }
+
+        @Override
+        boolean hasNext() {
+            return current + offset < 64 && SQUARES[current + offset].getFileIndex() != 7;
+        }
+    }
+
+    private static class WestRayGenerator extends RayGenerator {
+
+        {
+            offset = -1;
+        }
+
+        @Override
+        boolean hasNext() {
+            return current + offset >= 0 && SQUARES[current + offset].getFileIndex() != 7;
+        }
+    }
+
+    private static class NorthWestRayGenerator extends RayGenerator {
+
+        {
+            offset = -9;
+        }
+
+        @Override
+        boolean hasNext() {
+            return current + offset >= 0 && SQUARES[current + offset].getFileIndex() != 7;
+        }
     }
 }
--- src/test/java/cz/janhrcek/chess/model/SquareTest.java
+++ src/test/java/cz/janhrcek/chess/model/SquareTest.java
@@ -1,5 +1,8 @@
 package cz.janhrcek.chess.model;
 
+import static cz.janhrcek.chess.model.Square.*;
+import static cz.janhrcek.chess.model.Square.Direction.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -12,45 +15,45 @@ public class SquareTest {
 
     @Test
     public void sqareMapsToIndex() {
-        assertEquals(0, Square.A8.getIndex());
-        assertEquals(7, Square.H8.getIndex());
-        assertEquals(56, Square.A1.getIndex());
-        assertEquals(63, Square.H1.getIndex());
+        assertEquals(0, A8.getIndex());
+        assertEquals(7, H8.getIndex());
+        assertEquals(56, A1.getIndex());
+        assertEquals(63, H1.getIndex());
     }
 
     @Test
     public void indexMapsToSquare() {
-        assertEquals(Square.A8, Square.valueOf(0));
-        assertEquals(Square.H8, Square.valueOf(7));
-        assertEquals(Square.A1, Square.valueOf(56));
-        assertEquals(Square.H1, Square.valueOf(63));
+        assertEquals(A8, Square.valueOf(0));
+        assertEquals(H8, Square.valueOf(7));
+        assertEquals(A1, Square.valueOf(56));
+        assertEquals(H1, Square.valueOf(63));
     }
 
     @Test
     public void fileIndexTest() {
-        assertEquals(0, Square.A8.getFileIndex());
-        assertEquals(0, Square.A1.getFileIndex());
-        assertEquals(4, Square.E5.getFileIndex());
-        assertEquals(7, Square.H2.getFileIndex());
+        assertEquals(0, A8.getFileIndex());
+        assertEquals(0, A1.getFileIndex());
+        assertEquals(4, E5.getFileIndex());
+        assertEquals(7, H2.getFileIndex());
     }
 
     @Test
     public void rankIndexTest() {
-        assertEquals(0, Square.A8.getRankIndex());
-        assertEquals(7, Square.A1.getRankIndex());
-        assertEquals(3, Square.E5.getRankIndex());
-        assertEquals(6, Square.H2.getRankIndex());
+        assertEquals(0, A8.getRankIndex());
+        assertEquals(7, A1.getRankIndex());
+        assertEquals(3, E5.getRankIndex());
+        assertEquals(6, H2.getRankIndex());
     }
 
     @Test
     public void isDarkTest() {
-        assertTrue(Square.A1.isDark());
-        assertTrue(Square.E3.isDark());
-        assertTrue(Square.H6.isDark());
+        assertTrue(A1.isDark());
+        assertTrue(E3.isDark());
+        assertTrue(H6.isDark());
 
-        assertFalse(Square.H1.isDark());
-        assertFalse(Square.E6.isDark());
-        assertFalse(Square.B3.isDark());
+        assertFalse(H1.isDark());
+        assertFalse(E6.isDark());
+        assertFalse(B3.isDark());
     }
 
     @Test
@@ -58,18 +61,27 @@ public class SquareTest {
         long bbRepresentingA8 = 0b10000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000L;
         Set<Square> a8Set = Square.toSquareSet(bbRepresentingA8);
         assertEquals(1, a8Set.size());
-        assertTrue(a8Set.contains(Square.A8));
+        assertTrue(a8Set.contains(A8));
 
         long bbRepresentingAFile = 0b10000000_10000000_10000000_10000000_10000000_10000000_10000000_10000000L;
         Set<Square> aFileSet = Square.toSquareSet(bbRepresentingAFile);
         assertEquals(8, aFileSet.size());
         assertTrue(aFileSet.containsAll(
-                Arrays.asList(Square.A1, Square.A2, Square.A3, Square.A4, Square.A5, Square.A6, Square.A7, Square.A8)));
+                Arrays.asList(A1, A2, A3, A4, A5, A6, A7, A8)));
 
         long bbRepresenting3rdRank = 0b00000000_00000000_00000000_00000000_00000000_11111111_00000000_00000000L;
         Set<Square> thirdRankSet = Square.toSquareSet(bbRepresenting3rdRank);
         assertEquals(8, thirdRankSet.size());
         assertTrue(thirdRankSet.containsAll(
-                Arrays.asList(Square.A3, Square.B3, Square.C3, Square.D3, Square.E3, Square.F3, Square.G3, Square.H3)));
+                Arrays.asList(A3, B3, C3, D3, E3, F3, G3, H3))); //TODO - migrate to assertJ
+    }
+
+    @Test
+    public void squareIterationTest() {
+        assertThat(A1.squaresTo(NORTH))
+                .contains(A2, A3, A4, A5, A6, A7, A8)
+                .hasSize(7);
+
+        //TODO - more tests for square iteration
     }
 }
